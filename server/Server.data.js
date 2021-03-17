5100b8c3c785f67175dc4a81ef59757b08c1c890
Artem Vovk <artem.vovk.m@gmail.com>
2021-03-17 13:38:40 +0200

    added UniqueValues

--- server/Server.data.js
+++ server/Server.data.js
@@ -1,6 +1,35 @@
+const state = [];
+
+function statusBody (request, response) {
+    const result = {};
+    result.count = state.length;
+
+    const sumAllArrays = state.reduce((accumulator, currentValue) => {
+        return accumulator + currentValue.length;
+    }, 0);
+
+    const flattedArray = state.flat();
+    const sumNumArray = flattedArray.reduce((acc, cur) => {
+        return acc + Number(cur);
+    }, 0);
+
+    let unique = flattedArray.filter((val, index, a) => a.indexOf(val) === index); 
+
+    result.arithmeticMean = sumNumArray / sumAllArrays
+    result.valuesTotalLength = sumAllArrays;
+    result.uniqueValues = unique;
+
+    response.send(result);
+};
+
+function updateBody (request, response) {
+    let bodyResult = request.body.values;
+    state.push(bodyResult);
+    response.send(bodyResult);
+};
 
 
-
-// module.exports = {
-
-// };
+module.exports = {
+    statusBody,
+    updateBody,
+};
